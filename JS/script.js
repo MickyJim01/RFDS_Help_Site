2130bdd4417a4c61034f6b13e024f64a7874f1e3
Michael McGuire <michael.mcguire4869+code@gmail.com>
2024-06-07 08:35:34 +0930

    updated nav bar again

--- JS/script.js
+++ JS/script.js
@@ -6,11 +6,13 @@ const re_dropdown = document.getElementById("re_drop");
 // when MS button is clicked, MS drop down appears
 ms_button.addEventListener("click", function () {
     ms_dropdown.classList.toggle("show");
+    ms_button.classList.toggle("active_nav_button");
 });
 
 // When RE button is clicked, RE dropdown appears
 re_button.addEventListener("click", function () {
     re_dropdown.classList.toggle("show");
+    re_button.classList.toggle("active_nav_button");
 });
 
 // maybe will be able to change so that if you click outside the menu then the drop down will close
